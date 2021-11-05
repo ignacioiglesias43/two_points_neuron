3f00cdfcd9de9a2f3685022e7c70ed19fd1f5c92
Ignacio Iglesias Campoy <iiglesiascampoy@gmail.com>
2021-11-04 23:46:50 -0700

    Point class

--- src/js/helpers/constants.js
+++ src/js/helpers/constants.js
@@ -6,3 +6,8 @@ const BIAS = 0.05;
 
 const xCenter = WIDTH / 2;
 const yCenter = HEIGHT / 2;
+
+const colors = {
+  1: [255, 0, 0],
+  2: [0, 255, 0],
+};
--- src/js/objects/Point.js
+++ src/js/objects/Point.js
@@ -1,1 +1,21 @@
-class Point {}
+class Point {
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
+
+  draw() {
+    push();
+    strokeWeight(3);
+    stroke(50);
+
+    /* if (this.type === 1) stroke(255, 0, 0);
+    if (this.type === 2) stroke(0, 255, 0); */
+
+    stroke(colors[this.type][0], colors[this.type][1], colors[this.type][2]);
+
+    point(this.x, this.y);
+    pop();
+  }
+}
