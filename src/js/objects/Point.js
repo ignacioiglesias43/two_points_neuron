dd5255e17c7a1cb5a4061317176494c49560f41e
Ignacio Iglesias Campoy <iiglesiascampoy@gmail.com>
2021-11-07 21:35:02 -0700

    Point class modified

--- src/js/objects/Point.js
+++ src/js/objects/Point.js
@@ -1,5 +1,5 @@
 class Point {
-  constructor(x, y, type) {
+  constructor(x, y, type = 1) {
     this.x = x;
     this.y = y;
     this.type = type;
@@ -10,10 +10,12 @@ class Point {
     strokeWeight(3);
     stroke(50);
 
-    /* if (this.type === 1) stroke(255, 0, 0);
-    if (this.type === 2) stroke(0, 255, 0); */
+    if (this.type === 1) stroke(255, 0, 0);
+    else if (this.type === 2) stroke(0, 255, 0);
+    else if (this.type === 3) stroke(0, 0, 255);
+    else stroke(80);
 
-    stroke(colors[this.type][0], colors[this.type][1], colors[this.type][2]);
+    // stroke(colors[this.type][0], colors[this.type][1], colors[this.type][2]);
 
     point(this.x, this.y);
     pop();
