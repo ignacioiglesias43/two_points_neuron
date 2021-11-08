f2c47c5bc92b0e5c866a7726f850d69692e6acb0
Ignacio Iglesias Campoy <iiglesiascampoy@gmail.com>
2021-11-08 03:03:43 -0700

    Get Point Type

--- src/js/helpers/functions.js
+++ src/js/helpers/functions.js
@@ -1,1 +1,12 @@
 const getRandom = (max = 1, min = 0) => Math.random() * (max - min) + min;
+
+const getPointType = (vals) => {
+  if (vals[0] === 1 && vals[1] === 0 && vals[2] == 0 && vals[3] == 0) return 1;
+  else if (vals[0] === 1 && vals[1] === 1 && vals[2] == 0 && vals[3] == 0)
+    return 2;
+  else if (vals[0] === 1 && vals[1] === 0 && vals[2] == 1 && vals[3] == 0)
+    return 3;
+  else if (vals[0] === 1 && vals[1] === 0 && vals[2] == 0 && vals[3] == 1)
+    return 4;
+  else return -1;
+};
